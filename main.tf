61c4a4977fcdac71f61ee025a03f91e5e0833590
rajeshprabhu18 <61199847+rajeshprabhu18@users.noreply.github.com>
2022-08-02 09:24:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,13 +9,13 @@ variable "awsprops" {
     publicip = true
     keyname = "nv"
     secgroupname = "TestingSecurityGroupp"
+   
   }
 }
 
 provider "aws" {
   region = lookup(var.awsprops, "region")
-  access_key = "AKIASJARONS2RCTELGKB"
-  secret_key = "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"
+  
 }
 
 resource "aws_security_group" "project-iac-sg" {
